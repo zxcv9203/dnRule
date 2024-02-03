f062e851b534b0b8bd9d059501ede2b3ec4c8bf2
yongckim <zxcv9203@naver.com>
2024-02-03 11:04:37 +0900

    :sparkles: github action 스크립트 추가

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.21.6-alpine AS builder
+
+WORKDIR /app
+
+COPY go.mod ./
+
+RUN go mod download
+
+COPY main.go .
+
+RUN CGO_ENABLED=0 go build -o /main
+
+FROM alpine
+
+COPY --from=builder /main /main
+
+ENTRYPOINT ["/main"]
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	token, err := getToken()
+
+	if err != nil {
+		log.Fatalf("토큰을 가져오는데 실패했습니다 [ %v ]", err)
+	}
+	fmt.Println(token)
+}
+
+func getToken() (string, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return "", fmt.Errorf("GITHUB_TOKEN이 존재하지 않습니다.")
+	}
+	return token, nil
+}
